b20539a4b73cf62b82c6d7593addaba6e63e62b4
Erika Flores <foglzerika@gmail.com>
2017-07-11 22:29:23 -0500

    terminando ejercicio

---
+++ server.js
@@ -0,0 +1,17 @@
+//Importamos express y path
+var express = require ('express');
+var path = require ('path');
+
+var app = express();
+
+app.get('/', (req, res ) =>{
+	res.send('Hola mundo!!!!');
+	
+res.sendFile(__dirname+ '/index.html');
+});
+
+app.listen(8080);
+
+
+app.use('/static', express.static(path.join (__dirname, 'node_modules')));
+app.use('/static', express.static(path.join(__dirname, 'assets')));
